cf3e45dcb97200c7b8f96f8154c0794792274e73
Prog- <elizardo.powell@uip.edu.pa>
2024-02-06 18:10:27 -0500

    Modificacion de Variable

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@
 public class Main {
     public static void main(String[] args) {
         int miVariableEntera = 10;
-        system.out.println(miVariableEntera);
-
+        System.out.println(miVariableEntera);
+        //Modificamos el valor de la Variable
+        miVariableEntera = 5;
+    }
 }
